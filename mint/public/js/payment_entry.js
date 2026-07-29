07b5788904bbdf9ff4f76471e6b97354ea973a8a
Gsus <gsus@example.com>
2026-07-29 09:52:53 -0400

    chore: remove payment_entry overrides from mint (moved to l10n_ve)

--- mint/public/js/payment_entry.js
+++ mint/public/js/payment_entry.js
@@ -1,24 +1,4 @@
 frappe.ui.form.on("Payment Entry", {
-	onload: function(frm) {
-		const original_set_current_exchange_rate = frm.events.set_current_exchange_rate;
-		frm.events.set_current_exchange_rate = function(frm, exchange_rate_field, from_currency, to_currency) {
-			if (!from_currency || !to_currency) return;
-			if (original_set_current_exchange_rate) {
-				return original_set_current_exchange_rate(frm, exchange_rate_field, from_currency, to_currency);
-			}
-		}
-	},
-	setup: function (frm) {
-		frm.set_query("paid_to", function () {
-			frm.events.validate_company(frm);
-			return {
-				filters: {
-					is_group: 0,
-					company: frm.doc.company,
-				},
-			};
-		});
-	},
 	refresh: function (frm) {
 		// Custom logic for Payment Entry
 	}
